71316a79b55002f8cbec56babd09e38a2d72b9d2
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-03-04 15:45:41 +0100

    frontend set up

---
+++ src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container">
+  <h1 class="text-center">About Domains</h1>
+  
+  <div class="row mt-3">
+    <div class="col-md-8 offset-md-2">
+      <p>
+        Ionic ID is a web3 name service for the Ionic community, where anyone can mint their very own web3 domain username 
+        with a .ion extension.
+      </p>
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "About"
+  }
+</script>
+
+<style scoped>
+p {
+  text-align: justify;
+  font-size: 1.1em;
+}
+
+h3 {
+  margin-top: 35px;
+}
+</style>
